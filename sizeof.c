2ddadaa147048b0483054b456b33742e1ce80be0
ramla02 <ramla252017@gmail.com>
2023-10-23 11:09:53 +0300

    updated

--- sizeof.c
+++ sizeof.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
 /**
  * main - Entry point
  * Return: 0
@@ -7,5 +8,9 @@
 int main(void)
 {
 printf("Storage size for int : %ld \n", sizeof(int));
+printf("Storage size for float : %ld \n", sizeof(float));
+printf("Minimum float positive value : %E \n", FLT_MIN);
+printf("Maximum float positive value : %E \n", FLT_MAX);
+printf("Precision value : %d \n", FLT_DIG);
 return (0);
 }
