ae3c3b2eeb39879a3b8237e020644978b1d3e89c
ramla02 <ramla252017@gmail.com>
2023-11-05 16:10:38 +0300

    escape sequence characters

---
+++ 4-tab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * The main function is the start of the program.
+ * The following codes is an example to show a few escape sequence characters.
+ * When the below code is compiled and executed, it produces tabbed string
+ * literals.
+ * Return: 0 for the successful execution.
+ */
+int main(void)
+{
+printf("Hello\tWorld\n\n");
+return (0);
+}
